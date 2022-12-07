ff42a2eb30bd3693c8051154cc05cf0684b51106
GigaOrts <ortsortskhoev95@gmail.com>
2022-12-07 15:12:02 +0300

    Update EnemyPool.cs

--- Assets/Scripts/EnemyPool/EnemyPool.cs
+++ Assets/Scripts/EnemyPool/EnemyPool.cs
@@ -1,6 +1,21 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyPool : MonoBehaviour
 {
+    public List<SpawnData> SpawnDatas;
+
     [SerializeField] private Enemies _spawnedEnemies;
+
+    public void InitializeSpawnData()
+    {
+        SpawnDatas = new List<SpawnData>();
+
+        int enemyTypesCount = System.Enum.GetNames(typeof(Enemies)).Length;
+
+        for (int i = 0; i < enemyTypesCount; i++)
+        {
+            SpawnDatas.Add(new SpawnData((Enemies)(1 << i)));
+        }
+    }
 }
