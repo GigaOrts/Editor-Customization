c0d0554bbaafe202e2654845d56a605a09ae7626
GigaOrts <ortsortskhoev95@gmail.com>
2022-12-08 10:44:25 +0300

    Update CameraTransitionEditor.cs

--- Assets/Scripts/Сamera Transition/CameraTransition Editor/CameraTransitionEditor.cs
+++ Assets/Scripts/Сamera Transition/CameraTransition Editor/CameraTransitionEditor.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+using System.Text.RegularExpressions;
+using System;
 
-public class CameraTransitionEditor : MonoBehaviour
+[CustomEditor(typeof(CameraTransition))]
+public class CameraTransitionEditor : Editor
 {
-    // Start is called before the first frame update
-    void Start()
+    private const string _enumFile = "RouteName";
+
+    private CameraTransition _cameraTransition;
+    private string _pathToEnumFile;
+    private string _routeName = "New Route Name";
+
+    private void OnEnable()
     {
-        
+        _cameraTransition = (CameraTransition)target;
+
+        _pathToEnumFile = AssetDatabase.GUIDToAssetPath(AssetDatabase.FindAssets(_enumFile)[0]);
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnInspectorGUI()
     {
-        
+        DrawNewClipSelection();
+    }
+
+    private void DrawNewClipSelection()
+    {
+        _routeName = EditorGUILayout.TextField("Name", _routeName);
+        DrawAddButton();
+    }
+
+    private void DrawAddButton()
+    {
+        if (GUILayout.Button("Add"))
+        {
+            AddRoute();
+        }
+    }
+
+    private void AddRoute()
+    {
+        if (_routeName == string.Empty)
+            return;
+
+        if (!Regex.IsMatch(_routeName, @"^[a-zA-Z][a-zA-Z0-9_]*$"))
+            return;
+
+        Array array = Enum.GetValues(typeof(RouteName));
+        if (array.Length != 0)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (_routeName == array.GetValue(i).ToString())
+                {
+                    Debug.LogError("The path with the same name already exists!");
+                    return;
+                }
+            }
+        }
     }
 }
